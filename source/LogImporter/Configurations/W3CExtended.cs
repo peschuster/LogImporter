ee3346f7cadd540f8eda60856e96024e68d54f6b
Peter Schuster <schuster.pe@gmail.com>
2012-02-29 20:48:08 +0100

    Added more fields to w3c configuration.

--- source/LogImporter/Configurations/W3CExtended.cs
+++ source/LogImporter/Configurations/W3CExtended.cs
@@ -20,19 +20,26 @@ namespace LogImporter.Configurations
             this.dateTimeConverter = new OptionalDateTimeConverter(this.stringConverter);
             this.intConverter = new OptionalIntegerConverter(this.stringConverter);
 
-            this.mapping = new Dictionary<string, Action<LogEntry, string>>
+            this.mapping = new Dictionary<string, Action<LogEntry, string>>(StringComparer.OrdinalIgnoreCase)
             {
                 { "date", (e, v) => this.AddDate(e, v) },
                 { "time", (e, v) => this.AddTime(e, v) },
                 { "s-ip", (e, v) => e.sIp = this.stringConverter.Transform(v) },
+                { "s-sitename", (e, v) => e.sSitename = this.stringConverter.Transform(v) },
+                { "s-computername", (e, v) => e.sComputername = this.stringConverter.Transform(v) },
+                { "s-port", (e, v) => e.sPort = this.intConverter.Transform(v) },
+                { "cs-host", (e, v) => e.csHost = this.stringConverter.Transform(v) },
                 { "cs-method", (e, v) => e.csMethod = this.stringConverter.Transform(v) },
                 { "cs-uri-stem", (e, v) => e.csUriStem = this.stringConverter.Transform(v) },
                 { "cs-uri-query", (e, v) => e.csUriQuery = this.stringConverter.Transform(v) },
-                { "s-port", (e, v) => e.sPort = this.intConverter.Transform(v) },
                 { "cs-username", (e, v) => e.csUsername = this.stringConverter.Transform(v) },
-                { "c-ip", (e, v) => e.cIp = this.stringConverter.Transform(v) },
+                { "cs-version", (e, v) => e.csVersion = this.stringConverter.Transform(v) },
                 { "cs(User-Agent)", (e, v) => e.csUserAgent = this.stringConverter.Transform(v) },
-                { "sc-status", (e, v) => e.scStatus= this.intConverter.Transform(v) },
+                { "cs(Cookie)", (e, v) => e.csCookie = this.stringConverter.Transform(v) },
+                { "cs(Referer)", (e, v) => e.csReferer = this.stringConverter.Transform(v) },
+                { "c-ip", (e, v) => e.cIp = this.stringConverter.Transform(v) },
+                { "sc-bytes", (e, v) => e.scBytes = this.intConverter.Transform(v) },
+                { "sc-status", (e, v) => e.scStatus = this.intConverter.Transform(v) },
                 { "sc-substatus", (e, v) => e.scSubStatus = this.intConverter.Transform(v) },
                 { "sc-win32-status", (e, v) => e.scWin32Status = this.intConverter.Transform(v) },
                 { "time-taken", (e, v) => e.TimeTaken = this.intConverter.Transform(v) }
