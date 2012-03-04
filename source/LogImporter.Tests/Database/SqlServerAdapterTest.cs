bc8ae0027f790805f3eb5d2eb5ee300d1d93f930
Peter Schuster <schuster.pe@gmail.com>
2012-03-04 20:08:37 +0100

    Added test for connection pool size.

---
+++ source/LogImporter.Tests/Database/SqlServerAdapterTest.cs
@@ -0,0 +1,21 @@
+﻿using LogImporter.Database;
+using Xunit;
+using Xunit.Extensions;
+
+namespace LogImporter.Tests.Database
+{
+    public class SqlServerAdapterTest
+    {
+        [Theory]
+        [InlineData("Data Source=myServerAddress;Initial Catalog=myDataBase;User Id=myUsername;Password=myPassword", 100)]
+        [InlineData("Data Source=myServerAddress;Initial Catalog=myDataBase;User Id=myUsername;Password=myPassword;Max Pool Size=5", 5)]
+        public void MaxConcurrentConnectionsTest(string connectionString, int expected)
+        {
+            var target = new SqlServerAdapter(connectionString, "MyTable");
+
+            int result = target.MaxConcurrentConnections;
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
